01f6fd33b6d33083233f35b2bab04d78dd5cd4f2
Agustin Goni <agusgoni1997@gmail.com>
2025-04-07 09:03:26 -0300

    feat: add loading sevice and component

---
+++ src/app/shared/components/loading/loading.component.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { LoadingService } from '../../../core/services/loading.service';
+import { AsyncPipe, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-loading',
+  standalone: true,
+  imports: [MatProgressSpinnerModule, NgIf, AsyncPipe],
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.scss'
+})
+export class LoadingComponent {
+  constructor(public loadingSrv: LoadingService) {}
+
+}
